0efb470858571eef2a66efd117f01ef6ae27cc1f
Carvajal-Daniel <carvajal.dev@hotmail.com>
2025-10-21 19:27:40 -0300

    feat: updated cors and router

--- src/App.ts
+++ src/App.ts
@@ -3,9 +3,6 @@ import * as express from "express";
 import * as cookieParser from "cookie-parser";
 import userRoutes from "./infra/routes/user.routes";
 import * as cors from "cors";
-import * as dotenv from "dotenv";
-
-dotenv.config(); 
 
 export class App {
   public app: Application;
@@ -13,30 +10,26 @@ export class App {
   constructor() {
     this.app = express();
 
+  
     this.app.set("trust proxy", 1);
 
     this.app.use(express.json());
     this.app.use(cookieParser());
 
-    // Lista de origens permitidas
-    const allowedOrigins = [
-      process.env.FRONTEND_DOMAIN,    
-      process.env.FRONTEND_DOMAIN_WWW, 
-      "http://localhost:3001"          
-    ];
+ const allowedOrigins = ["https://uplys.com.br", "https://www.uplys.com.br", "localhost:5800"];
+this.app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Postman ou SSR
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error(`CORS bloqueado para origem: ${origin}`));
+    },
+    credentials: true,
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"],
+  })
+);
 
-    this.app.use(
-      cors({
-        origin: (origin, callback) => {
-          if (!origin) return callback(null, true); // Postman ou SSR
-          if (allowedOrigins.includes(origin)) return callback(null, true);
-          return callback(new Error(`CORS bloqueado para origem: ${origin}`));
-        },
-        credentials: true, // necessário para cookies HttpOnly
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-      })
-    );
 
     this.setupRoutes();
   }
